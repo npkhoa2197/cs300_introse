79d479a67ef46ef1d4ff45d00e6dbe4347fe40ba
ntban15 <ngtab.an.9@gmail.com>
2017-11-05 19:26:44 +0700

    Add Unity project to main repo

---
+++ src/ARMenu/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+	//code adapted from https://www.youtube.com/watch?v=S3pjBQObC90
+
+	float rotSpeed = 100;
+
+	void OnMouseDrag () {
+		//Only rotate horizontally
+		float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+
+		transform.Rotate(Vector3.up * -rotX);
+	}
+
+}
