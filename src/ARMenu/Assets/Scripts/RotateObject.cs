cf9f4ba15c142f30f7d7f79e684490ee3069f771
ntban15 <ngtab.an.9@gmail.com>
2017-11-15 19:02:08 +0700

    Change rotate mode back to transform.Rotate

--- src/ARMenu/Assets/Scripts/RotateObject.cs
+++ src/ARMenu/Assets/Scripts/RotateObject.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 //this script allows 3D objects with a box collider
 //to rotate horizontally
 public class RotateObject : MonoBehaviour {
-	//code adapted from https://www.youtube.com/watch?v=S3pjBQObC90
 
-	public float rotSpeed = 100;
-
-	private Rigidbody rigidbody;
-
-	void Start () {
-		rigidbody = GetComponent<Rigidbody>();
-	}
+	public float amount = 20f;
 
 	void OnMouseDrag () {
 		//Only rotate horizontally
-		float rotX = Input.GetAxis("Horizontal") * rotSpeed;// * Mathf.Deg2Rad;
-
-		rigidbody.AddTorque(transform.up * rotX);
+		float h = Input.GetAxis("Mouse X") * amount * Mathf.Deg2Rad;
+        transform.Rotate(transform.up * -h);
 	}
 }
