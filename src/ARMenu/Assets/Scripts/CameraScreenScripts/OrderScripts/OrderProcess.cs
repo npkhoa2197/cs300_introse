a9f6e778995675b6fdde62fc9d5cefa83420dd16
ntban15 <ngtab.an.9@gmail.com>
2017-12-04 23:08:00 +0700

    Push Order to Firebase
    - Remove unecessary JSON package

--- src/ARMenu/Assets/Scripts/CameraScreenScripts/OrderScripts/OrderProcess.cs
+++ src/ARMenu/Assets/Scripts/CameraScreenScripts/OrderScripts/OrderProcess.cs
@@ -6,28 +6,31 @@ using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
 using AssemblyCSharp;
-using Newtonsoft.Json;
+using System;
+
 public class OrderProcess : MonoBehaviour {
 
 	private GameObject canvas; 
 	private InputField quantityInput; 
 	private InputField requirementsInput; 
-	private DatabaseReference reference;
+	private DatabaseReference rootRef;
 	private FoodInfo foodInfo;
 	private VariantAdapter foodVariant;
 
 	// Use this for initialization
 	void Start () {
-		// Referencing the order canvas and the current model
+		// Referencing the order canvas and the current model and the input fields
 		canvas = GameObject.Find ("OrderCanvas");
 		foodInfo = transform.parent.GetComponent<FoodInfo> ();
 		foodVariant = transform.parent.GetComponent<VariantAdapter> ();
+		quantityInput = GameObject.Find ("QuantityInput").GetComponent<InputField> (); 
+		requirementsInput = GameObject.Find ("RequirementsInput").GetComponent<InputField> ();
 
 		// Set up the Editor before calling into the realtime database.
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://armenu-2220c.firebaseio.com/");
 
 		// Get the root reference location of the database.
-		reference = FirebaseDatabase.DefaultInstance.RootReference;
+		rootRef = FirebaseDatabase.DefaultInstance.RootReference;
 	}
 
 	// Update is called once per frame
@@ -37,42 +40,27 @@ public class OrderProcess : MonoBehaviour {
 
 	public void onCloseButtonClicked () {
 		canvas.SetActive (false);
+		quantityInput.text = "";
+		requirementsInput.text = "";
 	}
 
 	public void onOrderButtonClicked () {
 		//get inputs from the input fields
-		quantityInput = GameObject.Find ("QuantityInput").GetComponent<InputField> (); 
-		requirementsInput = GameObject.Find ("RequirementsInput").GetComponent<InputField> ();
-
 		string quantity = quantityInput.text;
 		string requirements = requirementsInput.text;
 
 		//create an Order object based on the information given by the users and the foodInfo
-		// Order order = new Order (requirements, false, foodInfo.getFoodName(), false, 
-		// 	foodInfo.getPrice(), long.Parse(quantity), 0);
+		Order order = new Order (requirements, false, foodInfo.getFoodName(), false, 
+			foodInfo.getPrice(), long.Parse(quantity), 0);
+		string jsonOrder = JsonUtility.ToJson(order);
 
-		//Debug.Log (order);
-
-		//string json = JsonConvert.SerializeObject (order);
-		long tableNumber = 0;
 		//write the new order as a new child node under Order entry
-		DatabaseReference _ref = reference.Child ("Order").Push();
-		_ref.Child ("finished").SetValueAsync (false);
-		_ref.Child ("paid").SetValueAsync (false);
-//		_ref.Child ("additionalRequirements").SetValueAsync (requirements);
-//		_ref.Child ("meal").SetValueAsync (foodInfo.getFoodName ());
-//		_ref.Child ("price").SetValueAsync (foodInfo.getPrice());
-//		_ref.Child ("quantity").SetValueAsync (long.Parse(quantity));
-//		_ref.Child ("tableNumber").SetValueAsync (tableNumber);
+		DatabaseReference _ref = rootRef.Child("Order").Push();
+		_ref.SetRawJsonValueAsync(jsonOrder);
 		
 		//after finishing the ordering, the order box will disappear
+		quantityInput.text = "";
+		requirementsInput.text = "";
 		canvas.SetActive (false);
-		Debug.Log(requirements);
-		Debug.Log(foodInfo.getFoodName());
-		Debug.Log(foodInfo.getPrice());
-		Debug.Log(long.Parse(quantity));
-		Debug.Log(tableNumber);
-
-		//Debug.Log(json);
 	}
 }
