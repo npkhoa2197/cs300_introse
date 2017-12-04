7d5216b6fc0b8c06fb2e091c6f71948168c063eb
npkhoa2197 <npkhoa@apcs.vn>
2017-12-04 21:29:25 +0700

    backup commit

--- src/ARMenu/Assets/Scripts/CameraScreenScripts/OrderScripts/OrderProcess.cs
+++ src/ARMenu/Assets/Scripts/CameraScreenScripts/OrderScripts/OrderProcess.cs
@@ -6,6 +6,7 @@ using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
 using AssemblyCSharp;
+using System;
 
 public class OrderProcess : MonoBehaviour {
 
@@ -18,10 +19,12 @@ public class OrderProcess : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// Referencing the order canvas and the current model
+		// Referencing the order canvas and the current model and the input fields
 		canvas = GameObject.Find ("OrderCanvas");
 		foodInfo = transform.parent.GetComponent<FoodInfo> ();
 		foodVariant = transform.parent.GetComponent<VariantAdapter> ();
+		quantityInput = GameObject.Find ("QuantityInput").GetComponent<InputField> (); 
+		requirementsInput = GameObject.Find ("RequirementsInput").GetComponent<InputField> ();
 
 		// Set up the Editor before calling into the realtime database.
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://armenu-2220c.firebaseio.com/");
@@ -37,12 +40,13 @@ public class OrderProcess : MonoBehaviour {
 
 	public void onCloseButtonClicked () {
 		canvas.SetActive (false);
+		quantityInput.text = "";
+		requirementsInput.text = "";
 	}
 
 	public void onOrderButtonClicked () {
 		//get inputs from the input fields
-		quantityInput = GameObject.Find ("QuantityInput").GetComponent<InputField> (); 
-		requirementsInput = GameObject.Find ("RequirementsInput").GetComponent<InputField> ();
+		
 
 		string quantity = quantityInput.text;
 		string requirements = requirementsInput.text;
@@ -50,18 +54,20 @@ public class OrderProcess : MonoBehaviour {
 		//create an Order object based on the information given by the users and the foodInfo
 		Order order = new Order (requirements, false, foodInfo.getFoodName(), false, 
 			foodInfo.getPrice(), long.Parse(quantity), 0);
-		
+
 		//write the new order as a new child node under Order entry
 		DatabaseReference _ref  = reference.Child ("Order").Push();
 		_ref.Child ("Additional Requirements").SetValueAsync (requirements);
 		_ref.Child ("Finished").SetValueAsync (false);
 		_ref.Child ("Meal").SetValueAsync (foodInfo.getFoodName ());
 		_ref.Child ("Paid").SetValueAsync (false);
-		_ref.Child ("Price").SetValueAsync (long.Parse (quantity));
-		_ref.Child ("Quantity").SetValueAsync (quantity);
-		_ref.Child ("Table").SetValueAsync (0);
+		_ref.Child ("Price").SetValueAsync (foodInfo.getPrice());
+		_ref.Child ("Quantity").SetValueAsync (Convert.ToInt64(quantity));
+		_ref.Child ("Table").SetValueAsync ("0");
 		
 		//after finishing the ordering, the order box will disappear
+		quantityInput.text = "";
+		requirementsInput.text = "";
 		canvas.SetActive (false);
 	}
 }
