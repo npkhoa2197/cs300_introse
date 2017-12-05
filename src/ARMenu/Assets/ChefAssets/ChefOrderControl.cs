91b65e54604749654cd26cbe2a5b9dad34abfc28
nxvinh2197 <nxvinh@apcs.vn>
2017-12-05 12:53:34 +0700

    update Chef handelOrder func

--- src/ARMenu/Assets/ChefAssets/ChefOrderControl.cs
+++ src/ARMenu/Assets/ChefAssets/ChefOrderControl.cs
@@ -19,15 +19,14 @@ public class ChefOrderControl : MonoBehaviour {
     public Text title;
     public List<orderItem> orderList;
     public orderItem itemOr;
-    
-
 	// Use this for initialization
 	void Start () {
         orderList = new List<orderItem>();
-        
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://armenu-2220c.firebaseio.com/");
         DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
-        FirebaseDatabase.DefaultInstance.GetReference("Order").GetValueAsync().ContinueWith(task => {
+        
+        FirebaseDatabase.DefaultInstance.GetReference("Order").ChildAdded += handleChildAdded;
+        /*FirebaseDatabase.DefaultInstance.GetReference("Order").GetValueAsync().ContinueWith(task => {
             if (task.IsFaulted) {
               // Handle the error...
             }
@@ -42,8 +41,9 @@ public class ChefOrderControl : MonoBehaviour {
                         (bool) dictOrder["finished"], (bool) dictOrder["paid"]);
                     orderList.Add(itemOr);   
                 }   
+                initListView(null, orderList);
             }
-        });
+        });*/
         
         Orders = new List<GameObject>();
         moveDisplacement = new List<float>();
@@ -60,17 +60,24 @@ public class ChefOrderControl : MonoBehaviour {
         offset = ((RectTransform)orderPrefab.transform).rect.height * 0.03f;
         orderHeight = ((RectTransform)orderPrefab.transform).rect.height * 0.65f + offset;
         Content.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, orderHeight * Orders.Count + 10);
-        
     }
-	/*var ref = FirebaseDatabase.DefaultInstance.GetReference("Order");
-        ref.ChildAdded += handleChildAdded;
-        void handleChildAdded(object sender, ChildChangedEventArgs args) {
-            if (args.DatabaseError != null) {
-                Debug.LogError(args.DatabaseError.Message);
-                return;
-            }
-            Debug.Log(args.Snapshot.Value);
-        }    */    
+	 
+    
+    void handleChildAdded(object sender, ChildChangedEventArgs args) {
+        
+        if (args.DatabaseError != null) {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        
+        IDictionary dictOrder = (IDictionary) args.Snapshot.Value;   
+        Debug.Log(dictOrder["meal"]);
+        itemOr = new orderItem (Convert.ToString(dictOrder["meal"]), Convert.ToString(dictOrder["additionalRequirements"]),
+            Convert.ToInt32(dictOrder["quantity"]), Convert.ToInt32(dictOrder["tableNumber"]), Convert.ToSingle(dictOrder["price"]), 
+            (bool) dictOrder["finished"], (bool) dictOrder["paid"]);
+        addOrder(null, itemOr);
+          
+    }       
 	private GameObject getNewOrder(){
 		return null;
 	}
@@ -85,51 +92,46 @@ public class ChefOrderControl : MonoBehaviour {
         return true;
     }
 
-    private void addOrder(Sprite image, List<orderItem> orderList)
+    private void addOrder(Sprite image, orderItem item)
     {
-        foreach (Transform child in Content.transform) {
-            Destroy(child.gameObject);
-        }
-        Orders = new List<GameObject>();
-        moveDisplacement = new List<float>();
+        
         Content.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, orderHeight * Orders.Count + 10);
-        foreach (orderItem item in orderList) {
+    
+        //Intantiate object
+        GameObject order = (GameObject)GameObject.Instantiate(orderPrefab);
 
-
-            //Intantiate object
-            GameObject order = (GameObject)GameObject.Instantiate(orderPrefab);
-
-            //set position
-            order.transform.SetParent(Content.transform);
-            order.transform.localScale = new Vector3(0.65f, 0.65f, 1);
-            if (Orders.Count != 0)
-            {
-                //wl("set local position");
-                order.transform.localPosition = new Vector3(Orders[Orders.Count - 1].transform.localPosition.x, Orders[Orders.Count - 1].transform.localPosition.y - orderHeight, 0);
-            }
-            else
-            {
-                order.transform.localPosition = new Vector3(401.015f, -orderHeight / 2, 0);
-            }
-
-            //set content
-            if (image != null)
-            order.transform.Find("Image").GetComponent<Image>().sprite = image;
-            order.transform.Find("Dishname").GetComponent<Text>().text = item.meal;
-            order.transform.Find("Info1").GetComponent<Text>().text = "Table "+ item.tableNumber.ToString() + " Amount: "+ item.quantity.ToString() + " Price: " +item.price.ToString()+"vnd";
-            order.transform.Find("Info2").GetComponent<Text>().text = "Additional requirement: "+ item.additionalRequirements;
-            order.transform.Find("CookDone").GetComponent<Button>().onClick.AddListener(() => onClickCooked());
-            order.GetComponent<Button>().onClick.AddListener(() => gameObject.GetComponent<OrderInfo>().ViewOrder());
-            Content.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, ((RectTransform)Content.transform).rect.height + orderHeight);
-
-            //add to orders list
-            Orders.Add(order);
-            moveDisplacement.Add(0);
+        //set position
+        order.transform.SetParent(Content.transform);
+        order.transform.localScale = new Vector3(0.65f, 0.65f, 1);
+        if (Orders.Count != 0)
+        {
+            //wl("set local position");
+            order.transform.localPosition = new Vector3(Orders[Orders.Count - 1].transform.localPosition.x, Orders[Orders.Count - 1].transform.localPosition.y - orderHeight, 0);
         }
+        else
+        {
+            order.transform.localPosition = new Vector3(401.015f, -orderHeight / 2, 0);
+        }
+
+        //set content
+        if (image != null)
+        order.transform.Find("Image").GetComponent<Image>().sprite = image;
+        order.transform.Find("Dishname").GetComponent<Text>().text = item.meal;
+        order.transform.Find("Info1").GetComponent<Text>().text = "Table "+ item.tableNumber.ToString() + " Amount: "+ item.quantity.ToString() + " Price: " +item.price.ToString()+"vnd";
+        order.transform.Find("Info2").GetComponent<Text>().text = "Additional requirement: "+ item.additionalRequirements;
+        order.transform.Find("CookDone").GetComponent<Button>().onClick.AddListener(() => onClickCooked());
+        order.GetComponent<Button>().onClick.AddListener(() => gameObject.GetComponent<OrderInfo>().ViewOrder());
+        Content.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, ((RectTransform)Content.transform).rect.height + orderHeight);
+
+        //add to orders list
+        Orders.Add(order);
+        moveDisplacement.Add(0);
+        
     }
 
     // Update is called once per frame
     void Update () {
+
 		if (getNewOrder() != null){
 			// add new order to list	
 		}
@@ -150,6 +152,7 @@ public class ChefOrderControl : MonoBehaviour {
                 }
 			}
 		}
+        
 	}
 
     public void onClickCooked()
@@ -180,10 +183,7 @@ public class ChefOrderControl : MonoBehaviour {
         removeFromDatabase(order);
     }
 
-    public void onClickAddOrder()
-    {
-        addOrder(null, orderList);
-    }
+    
     public class orderItem {
         public string meal, additionalRequirements;
         public int quantity, tableNumber;
