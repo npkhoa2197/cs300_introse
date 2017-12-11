737383d64c9dab48b3e9d821758e0662028a41c6
ntban15 <ngtab.an.9@gmail.com>
2017-12-11 12:54:58 +0700

    Revert "Revert "Merge pull request #19 from npkhoa2197/nktuan""
    This reverts commit dee188b0b225946c523760f8a7c7fe4112140476.

---
+++ src/ARMenu/Assets/MenuAssets/MenuInfo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInfo : MonoBehaviour {
+
+    private GameObject menuinfo;
+    private float posx, nextx;
+    private bool viewinfo;
+    private bool viewlist;
+    private float v;
+    public float vx;
+
+    //DEBUG
+    void wl(string s)
+    {
+        Debug.Log(s, gameObject);
+    }
+	// Use this for initialization
+	void Start () {
+        viewinfo = false;
+        menuinfo = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (viewinfo || viewlist)
+        {
+            posx = menuinfo.transform.position.x;
+            if ((viewinfo && (posx < nextx)) || (viewlist && (posx > nextx)))
+            {
+                menuinfo.transform.Translate(nextx - posx, 0, 0);
+                viewinfo = false;
+                viewlist = false;
+            }
+            else
+            {
+                menuinfo.transform.Translate(v, 0, 0);
+            }
+        }
+	}
+
+	private bool updateMenuItemDetail(){
+		wl("updateMenuItemDetail");
+		return(true);
+	}
+
+    public void ViewMenuItem()
+    {
+    	if (updateMenuItemDetail()){
+        	menuinfo = GameObject.Find("MenuDetail");
+        	posx = menuinfo.transform.position.x;
+        	nextx = Screen.width / 2;
+        	v = -vx;
+        	viewinfo = true;
+    	}
+    }
+
+    public void ViewMenuList()
+    {
+        menuinfo = GameObject.Find("MenuDetail");
+        posx = menuinfo.transform.position.x;
+        nextx = Screen.width / 2 * 3;
+        v = vx;
+        viewlist = true;
+    }
+}
