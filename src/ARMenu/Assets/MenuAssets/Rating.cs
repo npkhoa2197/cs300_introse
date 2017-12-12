3b59daa8a8f71f4628351b2185c49901efb573ca
Tuan <tuanbi97@gmail.com>
2017-12-12 11:06:44 +0700

    Update MenuScreen

---
+++ src/ARMenu/Assets/MenuAssets/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Rating : MonoBehaviour {
+
+    public float scorevalue = 0.3f;
+	// Use this for initialization
+	void Start () {
+        Slider score = this.transform.Find("Score").GetComponent<Slider>();
+        score.value = scorevalue;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+    
+}
