645f959fef2986b8b4fc4902e20016bbbddbf2f2
ntban15 <ngtab.an.9@gmail.com>
2017-12-15 09:48:05 +0700

    Add test for ChefScreen

---
+++ src/ARMenu/Assets/Scripts/TestAutomationScripts/ChefScreenTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class ChefScreenTest : UITest {
+
+	[UnityTest]
+	public IEnumerator CanReceiveOrders() {
+		//open Chef scene
+		yield return LoadScene("ChefScreen");
+		
+		//check if any order appears on screen
+		yield return WaitFor(new ObjectAppeared("OrderItem(Clone)"));
+	}
+	
+	[UnityTest]
+	public IEnumerator CanNavigateDetails() {
+		//open Chef scene
+		yield return LoadScene("ChefScreen");
+
+		//check if any order appears on screen
+		yield return WaitFor(new ObjectAppeared("OrderItem(Clone)"));
+
+		//click that order
+		yield return Press("OrderItem(Clone)");
+	}
+
+}
