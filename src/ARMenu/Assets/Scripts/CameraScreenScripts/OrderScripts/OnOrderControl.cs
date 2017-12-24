a4903401bfb541faedb2c62afe16301d51c89b55
ntban15 <ngtab.an.9@gmail.com>
2017-12-24 15:08:18 +0700

    Add Details for camera screen

--- src/ARMenu/Assets/Scripts/CameraScreenScripts/OrderScripts/OnOrderControl.cs
+++ src/ARMenu/Assets/Scripts/CameraScreenScripts/OrderScripts/OnOrderControl.cs
@@ -30,7 +30,7 @@ public class OnOrderControl : MonoBehaviour {
     void Start () {
 		foodManager = transform.parent.GetComponent<FoodTargetManager> ();
 
-		canvas = GameObject.Find ("OrderCanvas");
+		canvas = this.gameObject;
 		detail = canvas.transform.Find("ScrollView_5/ScrollRect/Content");
 		//quantityInput = GameObject.Find ("Amount").GetComponent<InputField> (); 
 		quantityInput = detail.Find("Amount").GetComponent<InputField>();
@@ -89,6 +89,7 @@ public class OnOrderControl : MonoBehaviour {
 	public void onOrderButtonClicked () {
 		//get inputs from the input fields
 		string quantity = quantityInput.text;
+		Debug.Log(quantity);
 		string requirements = requirementsInput.text;
 
 		//create an Order object based on the information given by the users and the FoodManager
@@ -108,59 +109,14 @@ public class OnOrderControl : MonoBehaviour {
 		_ref.SetRawJsonValueAsync(jsonOrder);
 		
 		//after finishing the ordering, the order box will disappear
-		quantityInput.text = "";
+		quantityInput.text = "1";
 		requirementsInput.text = "";
-		canvas.SetActive (false);
+		//canvas.SetActive (false);
 	}
 
 	public void onQuantityChanged() {
 		double totalPrice = double.Parse(quantityInput.text)*foodManager.GetFoodPrice();
 		detail.Find("Total").GetComponent<Text>().text = totalPrice.ToString() + "$";
-	}
-
- //    public void setOptions(GameObject optionprefab, GameObject DishContent)
- //    {
- //    	GameObject optionsContent = DishContent.transform.Find("OptionList/ScrollRect/Content").gameObject;
- //        for (int i = 0; i < optionsContent.transform.childCount; i++)
- //        {
- //        	Destroy(optionsContent.transform.GetChild(i).gameObject);
-	// 	}
- //        optionlist = new List<GameObject>();
- //        optionsContent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0);
- //        for (int i = 0; i < content.options.Count; i++)
- //        {
- //        	GameObject option = GameObject.Instantiate(optionprefab);
- //            option.transform.SetParent(optionsContent.transform);
- //            option.transform.localScale = new Vector3(1, 1, 1);
- //            option.transform.localPosition = new Vector3(i*360 + 60, -50, 0);
- //            option.transform.Find("Text").GetComponent<Text>().text = content.options[i];
- //            option.GetComponent<Toggle>().isOn = false;
- //            optionsContent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, ((RectTransform)optionsContent.transform).rect.width + 360);
- //            optionlist.Add(option);
- //        }
-	// }
-
-	//initialize comments in the detail screen
-	public void setComments(GameObject commentprefab, GameObject DishContent)
-	{
-		GameObject commentsContent = DishContent.transform.Find("CommentList/ScrollRect/Content").gameObject;
-		for (int i = 0; i < commentsContent.transform.childCount; i++)
-		{
-        	Destroy(commentsContent.transform.GetChild(i).gameObject);
-		}
-		commentlist = new List<GameObject>();
-        commentsContent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0);
-        for (int i = 0; i < content.comments.Count; i++)
-        {
-        	GameObject comment = GameObject.Instantiate(commentprefab);
-            comment.transform.SetParent(commentsContent.transform);
-            comment.transform.localScale = new Vector3(1, 1, 1);
-            comment.transform.localPosition = new Vector3(658.7f * i + 311, -174.4f, 0);
-            comment.transform.Find("Writer").GetComponent<Text>().text = content.comments[i].Item1;
-            comment.transform.Find("Text").GetComponent<Text>().text = content.comments[i].Item2;
-            commentsContent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, ((RectTransform)commentsContent.transform).rect.width + 658.7f);
-            commentlist.Add(comment);
-		}
 	}
 
     public void setContent(DishContent _content)
@@ -170,22 +126,9 @@ public class OnOrderControl : MonoBehaviour {
         //Set content
        	detail.localPosition = new Vector3(detail.localPosition.x, 0, detail.localPosition.z);
         canvas.transform.Find("Title/Text").GetComponent<Text>().text = content.dishname;
-        
-        if (content.image == null) detail.Find("Image").GetComponent<Image>().color = Color.black;
-        else detail.Find("Image").GetComponent<Image>().sprite = content.image;
-        
-        detail.Find("Rating").GetComponent<Rating>().setValue(content.score);
-        detail.Find("Description").GetComponent<Text>().text = content.description;
-        
-        //Options content
-        //setOptions(optionprefab, Content.gameObject);
-        
         detail.Find("Price").GetComponent<Text>().text = content.price.ToString() + "$";
         detail.Find("Amount").Find("Placeholder").GetComponent<Text>().text = content.amount.ToString();
         detail.Find("Total").GetComponent<Text>().text = (content.price * content.amount).ToString() + "$";
         detail.Find("AdditionalInfo").GetComponent<InputField>().text = content.additionalinfo;
-        
-        //Comments content
-        setComments(commentprefab, detail.gameObject);
     }
 }
