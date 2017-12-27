34e38ae9f95d1e88519acd39ed8c1c38b26dbf63
nxvinh2197 <nxvinh@apcs.vn>
2017-12-27 08:04:39 +0700

    update use-case spec, cloud function

--- src/Cloud Function/index.js
+++ src/Cloud Function/index.js
@@ -1,9 +1,24 @@
-const functions = require('firebase-functions');
-
-
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
+const functions = require('firebase-functions');
 
+exports.calAveRate = functions.database.ref('Meal/{mealID}/Rating/{ratingID}').onWrite(event => {
+	let promises = [];
+	event.data.ref.parent.parent.child('AveRating').once('value').then(snap => {
+      oldAve = parseFloat(snap.val().Rate);
+      noOfRating = parseFloat(snap.val().NoOfRating);   
+      newRate = parseFloat(event.data.val());
+      newAve = (oldAve * noOfRating + newRate)/(noOfRating+1);
+      noOfRating +=1;
+
+      promises.push(event.data.ref.parent.parent.child('AveRating/Rate').set(newAve));
+	  promises.push(event.data.ref.parent.parent.child('AveRating/NoOfRating').set(noOfRating));
+    });
+	
+	
+	return Promise.all(promises);
+	
+});
 exports.deletePaidOrder = functions.database.ref('FinishedOrder/{orderID}').onWrite(event => {
 	let promises = [];
 	if (event.data.val().paid == true) {
